6f1811262c2b4a56d6099d80f3345f74b41dc44b
sthaarekh <arekhshrestha@gmail.com>
2025-02-17 23:19:21 +0545

    Toast added to reset-password

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { Eye } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
 
 const ResetPassword = () => {
   const { token } = useParams();
+  const [showPassword, setShowPassword] = useState(false);
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const navigate = useNavigate();
@@ -18,21 +21,21 @@ const ResetPassword = () => {
       const host = "http://localhost:5001";
       console.log("Sending request to:", `${host}/api/v1/clubs/reset-password/${token}`);
       console.log("New Password:", newPassword);
-  
+      const loadingToastId = toast.loading("Reseting the password...");
+
       const response = await fetch(`${host}/api/v1/clubs/reset-password/${token}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ newPassword :newPassword }),
       });
-  
       const data = await response.json();
-      console.log("Response:", data);
+      toast.dismiss(loadingToastId);
   
       if (response.ok) {
-        alert("Password reset successful!");
+        toast.success("Password reset successful!");
         navigate("/login");
       } else {
-        alert(data.error || "Error resetting password.");
+        toast.error("Error reseting password.");
       }
     } catch (error) {
       console.error("Fetch error:", error);
@@ -45,20 +48,23 @@ const ResetPassword = () => {
       <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6">
         <h2 className="text-2xl font-semibold text-center text-gray-900 mb-4">Reset Password</h2>
         <form onSubmit={handleResetPassword} className="space-y-4">
-          <div>
+          <div className="relative">
             <label className="block text-sm font-medium text-gray-700">New Password</label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               className="w-full px-3 py-2 border rounded-lg"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
               required
             />
+            <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-10 -translate-y-1/2 text-slate-400 hover:text-slate-600">
+                <Eye className="h-5 w-5" />
+            </button>
           </div>
-          <div>
+          <div className="relative">
             <label className="block text-sm font-medium text-gray-700">Confirm Password</label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               className="w-full px-3 py-2 border rounded-lg"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
